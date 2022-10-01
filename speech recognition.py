04b597ef963bd56a800bcb8a2f16408aaabc0058
djhavar <90192071+djhavar@users.noreply.github.com>
2022-10-01 16:54:36 +0530

    Add files via upload

--- speech recognition.py
+++ speech recognition.py
@@ -1,5 +1,5 @@
-from tkinter import*
-import speed_recognition as sr
+from tkinter import *
+import speech_recognition as sr
 import webbrowser
 import pyttsx3
 from datetime import datetime
@@ -7,9 +7,9 @@ import subprocess
 
 root =Tk() 
 root.geometry("500x500")
-root.configure(bg="Light Blue")
+root.configure(background="Light Blue")
 
-label=Label(root,text="Welcome To Your Personal Desktop Assistant",
+label=Label(root,text="Welcome To Your Personal Desktop Assistant")
 label.place(relx=0.5,rely=0.1,anchor=CENTER)
 
 text_to_speech=pyttsx3.init()
@@ -22,7 +22,7 @@ def r_audio():
     speech_recognisor= sr.Recognizer()
     speak("How can i help you..")
     with sr.Microphone() as source:
-        audio= speech recognizer.listen(source)
+        audio= speech_recognizer.listen(source)
         voice_data=''
         
     try:
@@ -58,11 +58,11 @@ def respond(voice_data):
         webbrowser.get().open("https://www.youtube.com/")
         
     if "text editor" in voice_data:
-            speak("opening_notpad")
-            print("opening_notpad")
-          subprocess.Popen("notpad.exe")
+            speak("opening_notepad")
+            print("opening_notepad")
+            subprocess.Popen["notepad.exe"]
     
-btn= Button(command=r_audio,bg="red3",fg="white,padx=10,pady=1",font=("Arial",11,"bold"))
+btn= Button(command=r_audio,bg="red3",fg="white",padx=10,pady=1,font=("Arial",11,"bold"))
 btn.place(relx=0.5,rely=0.5,anchor=CENTER)
 
 root.mainloop()
